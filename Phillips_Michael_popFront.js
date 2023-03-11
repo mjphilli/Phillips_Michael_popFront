2b1ac4684be01dcca186920481ebc19920eba6a1
mjphilli <phillipsjmichael@gmail.com>
2023-03-10 22:16:27 -0600

    First commit, just comments

---
+++ Phillips_Michael_popFront.js
@@ -0,0 +1,3 @@
+// Given an array, remove and return the value at the beginning of the array. Do this without using any built-in array methods except pop().
+
+// For example, popFront([5, 93, 22, 4]) should return 5 and the original array should result as [93, 22, 4].
